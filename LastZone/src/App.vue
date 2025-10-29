f4077478dd68a770328c26b0983370b0c2192206
OstrovskyIv <ostrovskyiml@gmail.com>
2025-10-29 19:43:46 +0300

    redesigned the project structure

--- LastZone/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <TheHeader />
-  <MainBody/>
-</template>
-
-<script setup lang="ts">
-import TheHeader from "@/components/layout/TheHeader.vue";
-import MainBody from "@/components/layout/MainBody.vue";
-</script>
--- LastZone/src/main.ts
+++
@@ -1,7 +0,0 @@
-import './assets/fonts.css'  // Сначала шрифты
-import './assets/main.css'   // Потом основные стили
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
