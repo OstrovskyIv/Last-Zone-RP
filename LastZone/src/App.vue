fe17b32a9e35a665764192b3e07d196b1a928179
OstrovskyIv <ostrovskyiml@gmail.com>
2025-10-29 19:24:20 +0300

    Adding a Project

---
+++ LastZone/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <TheHeader />
+  <MainBody/>
+</template>
+
+<script setup lang="ts">
+import TheHeader from "@/components/layout/TheHeader.vue";
+import MainBody from "@/components/layout/MainBody.vue";
+</script>
